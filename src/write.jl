3c27fcf34ea1b6bc6f461a69473f2b65b019640c
OTDE <mail@sethchapman.com>
2020-02-28 15:18:59 -0800

    Revise `write` related functions for readability
    All `write` calls in `EDF.write` now have a clear lowest entry point, where all high-level
    structures in `EDF` call into `write_bytes` or `write_padded`, which dispatches in turn on
    `write_bytes`. This also allows us to dispatch `write_bytes` on annotation type, reducing
    code repetition.
    
    Additionally, variables are given more descriptive names.

--- src/write.jl
+++ src/write.jl
@@ -2,39 +2,44 @@
 ##### Writing EDFs
 #####
 
-function write_value(io::IO, value::T) where T<:Union{PatientID,RecordingID}
-    nb = 0
-    for f in fieldnames(T)
-        x = getfield(value, f)
-        if T <: RecordingID && f === :startdate
-            nb += write_value(io, "Startdate ")
-        end
-        nb += write_value(io, x) + Base.write(io, 0x20)
-    end
-    return nb
-end
+#####
+##### `write_bytes`
+#####
 
-function write_annotations(io::IO, record::DataRecord, max_bytes::Integer)
+write_bytes(io::IO, value) = Base.write(io, string(value))
+write_bytes(io::IO, date::Date) = Base.write(io, uppercase(Dates.format(date, dateformat"dd-u-yyyy")))
+write_bytes(io::IO, date::DateTime) = Base.write(io, Dates.format(date, dateformat"dd\.mm\.yyHH\.MM\.SS"))
+write_bytes(io::IO, ::Missing) = Base.write(io, "X")
+write_bytes(io::IO, x::AbstractFloat) = Base.write(io, string(isinteger(x) ? trunc(Int, x) : x))
+write_bytes(io::IO, continuous::Bool) = Base.write(io, continuous ? "EDF+C" : "EDF+D")
+write_bytes(::IO, ::Nothing) = 0
+
+function write_bytes(io::IO, metadata::T) where T<:Union{PatientID,RecordingID}
     bytes_written = 0
-    for annotation in record
-        bytes_written += write_annotation(io, annotation)
+    for name in fieldnames(T)
+        field = getfield(metadata, name)
+        if T <: RecordingID && name === :startdate
+            bytes_written += write_bytes(io, "Startdate ")
+        end
+        bytes_written += write_bytes(io, field) + Base.write(io, 0x20)
     end
-    while bytes_written < max_bytes
-        bytes_written += Base.write(io, 0x0)
-    end
-    bytes_written == max_bytes || error("Number of bytes written does not match the size of the data record")
     return bytes_written
 end
 
-write_value(io::IO, value) = Base.write(io, string(value))
-write_value(io::IO, date::Date) = Base.write(io, uppercase(Dates.format(date, dateformat"dd-u-yyyy")))
-write_value(io::IO, ::Missing) = Base.write(io, "X")
-write_value(io::IO, x::AbstractFloat) = Base.write(io, string(isinteger(x) ? trunc(Int, x) : x))
+function write_bytes(io::IO, annotations::T) where {T<:Union{DataRecord, Vector{<:AbstractAnnotation}}}
+    bytes_written = 0
+    for annotation in annotations
+        bytes_written += write_bytes(io, annotation)
+    end
+    return bytes_written
+end
 
-function write_annotation(io::IO, annotation::AbstractAnnotation)
-    bytes_written = write_value(io, sign(annotation)) + write_value(io, annotation.offset)
+function write_bytes(io::IO, annotation::AbstractAnnotation)
+    bytes_written = Base.write(io, sign(annotation)) +
+                    write_bytes(io, annotation.offset)
     if has_duration(annotation)
-        bytes_written += Base.write(io, 0x15) + write_value(io, annotation.duration)
+        bytes_written += Base.write(io, 0x15) +
+                         write_bytes(io, duration(annotation))
     end
     bytes_written += Base.write(io, footer(annotation)...)
     if has_events(annotation)
@@ -46,89 +51,84 @@ function write_annotation(io::IO, annotation::AbstractAnnotation)
     return bytes_written
 end
 
-function write_annotation(io::IO, annotations::Vector{<:AbstractAnnotation})
-    bytes_written = 0
-    for annotation in annotations
-        bytes_written += write_annotation(io, annotation)
-    end
-    return bytes_written
-end
-
-write_annotation(::IO, ::Nothing) = 0
-
 sign(annotation::AbstractAnnotation) = annotation.offset >= 0 ? '+' : '-'
 
 has_duration(annotation::RecordAnnotation) = false
 has_duration(annotation::TimestampAnnotation) = annotation.duration !== nothing
 
-duration(annotation::RecordAnnotation) = missing
+duration(annotation::RecordAnnotation) = nothing
 duration(annotation::TimestampAnnotation) = annotation.duration
 
 footer(annotation::RecordAnnotation) = (0x14, 0x14)
 footer(annotation::TimestampAnnotation) = 0x14
 
-has_events(annotation::RecordAnnotation) = true
-has_events(annotation::TimestampAnnotation) = annotation.events !== nothing
+has_events(annotation::RecordAnnotation) = annotation.events !== nothing
+has_events(annotation::TimestampAnnotation) = true
 
-function write_padded(io::IO, value, n::Integer)
-    b = write_value(io, value)
-    @assert b <= n
-    while b < n
-        b += Base.write(io, 0x20)
+function write_padded(io::IO, value, byte_limit::Integer; pad::UInt8=0x20)
+    bytes_written = write_bytes(io, value)
+    bytes_written <= byte_limit || error("Written value $value contains more bytes than limit $byte_limit")
+    while bytes_written < byte_limit
+        bytes_written += Base.write(io, pad)
     end
-    return b
-end
-
-function write_file_header(io::IO, header::FileHeader, signal_count::Integer)
-    return nothing
+    return bytes_written
 end
 
 function write_header(io::IO, file::File)
-    h = file.header
-    has_anno = file.annotations !== nothing
-    signal_count = h.n_signals + has_anno
-    b = write_padded(io, h.version, 8) +
-        write_padded(io, h.patient, 80) +
-        write_padded(io, h.recording, 80) +
-        write_padded(io, Dates.format(h.start, dateformat"dd\.mm\.yyHH\.MM\.SS"), 16) +
-        write_padded(io, 256 * (signal_count + 1), 8) +
-        write_padded(io, h.continuous ? "EDF+C" : "EDF+D", 44) +
-        write_padded(io, h.n_records, 8) +
-        write_padded(io, h.duration, 8) +
-        write_padded(io, signal_count, 4)
-    pads = [16, 80, 8, 8, 8, 8, 8, 80, 8]
-    for (i, w) in zip(1:fieldcount(SignalHeader), pads)
-        for s in file.signals
-            h = s.header
-            b += write_padded(io, getfield(h, i), w)
+    has_annotations = file.annotations !== nothing
+    signal_count = length(file.signals) + has_annotations
+    bytes_written = write_file_header(io, file.header, signal_count) +
+                    write_signal_headers(io, file, has_annotations)
+    reserved_bytes = 32 * signal_count
+    bytes_written += write_padded(io, 0x20, reserved_bytes)
+    return bytes_written
+end
+
+function write_file_header(io::IO, header::FileHeader, signal_count::Integer)
+    total_header_bytes = 256 * (signal_count + 1)
+    return write_padded(io, header.version, 8) +
+           write_padded(io, header.patient, 80) +
+           write_padded(io, header.recording, 80) +
+           write_padded(io, header.start, 16) +
+           write_padded(io, total_header_bytes, 8) +
+           write_padded(io, header.continuous, 44) +
+           write_padded(io, header.n_records, 8) +
+           write_padded(io, header.duration, 8) +
+           write_padded(io, signal_count, 4)
+end
+
+const SIGNAL_HEADER_PADS = [16, 80, 8, 8, 8, 8, 8, 80, 8]
+
+function write_signal_headers(io::IO, file::File, has_annotations::Bool)
+    bytes_written = 0
+    for (field, padding) in zip(1:fieldcount(SignalHeader), SIGNAL_HEADER_PADS)
+        for signal in file.signals
+            header = signal.header
+            bytes_written += write_padded(io, getfield(header, field), padding)
         end
-        if has_anno
-            h = file.annotations.header
-            b += write_padded(io, getfield(h, i), w)
+        if has_annotations
+            header = EDF.SignalHeader(file.annotations.header)
+            bytes_written += write_padded(io, getfield(header, field), padding)
         end
     end
-    ns = 32 * (length(file.signals) + has_anno)
-    for _ = 1:ns
-        b += Base.write(io, 0x20)
-    end
-    return b
+    return bytes_written
 end
 
 function write_data(io::IO, file::File)
-    b = 0
+    bytes_written = 0
     max_bytes = file.annotations.header.n_samples * 2
-    for i in 1:file.header.n_records
+    for record_index in 1:file.header.n_records
         for signal in file.signals
-            n = signal.header.n_samples
-            s = (i - 1) * n
-            stop = min(s + n, length(signal.samples))
-            b += Base.write(io, view(signal.samples, s+1:stop))
+            sample_count = signal.header.n_samples
+            start = (record_index - 1) * sample_count
+            stop = min(start + sample_count, length(signal.samples))
+            bytes_written += Base.write(io, view(signal.samples, (start + 1):stop))
         end
         if file.annotations !== nothing
-            b += write_annotations(io, file.annotations.records[i], max_bytes)
+            bytes_written += write_padded(io, file.annotations.records[record_index], max_bytes; pad=0x0)
         end
     end
-    return b
+    return bytes_written
 end
 
 """
@@ -139,4 +139,4 @@ Write the given `EDF.File` object to the given stream or file and return the num
 bytes written.
 """
 write(io::IO, file::File) = write_header(io, file) + write_data(io, file)
-write(path::AbstractString, edf::File) = open(io->write(io, edf), path, "w")
+write(path::AbstractString, edf::File) = open(io -> write(io, edf), path, "w")

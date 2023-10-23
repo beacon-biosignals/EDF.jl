75022dce21c96dc71c7eddeda9b67d04e5355574
Phillip Alday <phillip.alday@beacon.bio>
2023-10-23 10:31:32 -0500

    wip

--- src/discontiguous.jl
+++ src/discontiguous.jl
@@ -26,55 +26,57 @@ function read_discontiguous_signals!(file::File)
     time_anns = file.signals[time_idx]
     # annotations have their own timestamps
     signals = filter(x -> isa(x, Signal), file.signals)
-    # @info "" time_anns
-    # record_stop = 1
-    # for record_index in 1:(file.header.record_count), (signal_idx, signal) in enumerate(file.signals)
-    #     if signal_idx != time_idx
-    #         record_stop += record_index * signal.header.samples_per_record - 1
-    #         continue
-    #     end
-    #     seek(file.io, record_stop)
-    #     read_signal_record!(file, signal, record_index)
-    #     break
-    # end
-    # # not quite right because of headers
-    # seekstart(file.io)
-    EDF.read!(file)
-    # gonna read and then resize in memory and then copy around
+    # XXX first we read, then we resize in memory, and finally copy around
     # not the most efficient route, but c'est la vie
-    # specific logic for discontinuous signals
+
+    # 1. read
+    EDF.read!(file)
+
     final_record_start = only(last(time_anns.records)).onset_in_seconds
     final_record_start = Int(final_record_start)
     for signal in signals
+        # 2. resize
         sr = sampling_rate(file, signal)
         total_length = final_record_start * sr + signal.header.samples_per_record
-        prev_length = length(signal.samples)
+        # prev_length = length(signal.samples)
         resize!(signal.samples, total_length)
-        # fill all the new stuff with 0s
-        samples = signal.samples
-        fill!(@view(samples[prev_length:end]), 0)
+        # # fill all the new stuff with 0s
+        # samples = signal.samples
+        # fill!(@view(samples[prev_length:end]), 0)
     end
 
     spr = file.header.seconds_per_record
+    # count is used for sanity checking that we foudn the correct number of
+    # discontinuities
     count = 0
 
+    # XXX Note that this logic will probably fail for truncated final records.
     for signal in signals
+        sr = sampling_rate(file, signal)
+        rec_n_samples = Int(sr * spr)
+        samples = signal.samples
         prev_start = only(first(time_anns.records)).onset_in_seconds
-        for tal in @view(time_anns.records[2:end])
+        for (tal_idx, tal) in enumerate(@view(time_anns.records[2:end]))
             start = only(tal).onset_in_seconds
-            if start - spr == prev_start
-                prev_start = start
-                continue
-            else
-                prev_start = start
-                # TODO copy around and fill source areas with 0s
-
+            if start - spr != prev_start
+                # 4. copy around
+                start_idx = tal_idx * rec_n_samples + 1
+                # -1 because 1-based indexing
+                end_idx = start_idx + rec_n_samples - 1
+                @info "" start_idx, end_idx
+                slice = view(samples, start_idx:end_idx)
+                @info "" slice
+                post_slice = view(samples, end_idx:lastindex(samples))
+                @info "" post_slice
+                copyto!(post_slice, slice)
+                fill!(slice, 0)
                 count += 1
             end
+            prev_start = start
         end
     end
 
-    count % length(signals) == 0 ||
+    !iszero(count) || count % length(signals) == 0 ||
         error("Found an unexpected number of discontinuities")
 
     return file

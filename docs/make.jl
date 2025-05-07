f11a092136cdbce26090b51dc0ef3eefc4f812b1
Phillip Alday <phillip.alday@beacon.bio>
2025-05-07 13:15:18 -0500

    add docs preview

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,7 @@ makedocs(; modules=[EDF],
          sitename="EDF.jl",
          authors="Beacon Biosignals, Inc.",
          checkdocs=:public,
+         push_preview=true,
          pages=["API" => "index.md"])
 
 deploydocs(; repo="github.com/beacon-biosignals/EDF.jl.git")
